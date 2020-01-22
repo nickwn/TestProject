82c65c9807be5124b73fa00f1bbb3b61d78944d1
nick <nick.thenebels@gmail.com>
2020-01-22 14:25:13 -0800

    added hi there

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.testproject;
 
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,11 +21,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Hi there!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
